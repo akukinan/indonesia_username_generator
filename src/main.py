7dade23680f4c41d1936fb7cfa90082158be08c9
akukinan <yosafatkinantan@gmail.com>
2024-11-13 10:34:34 +0700

    Fixed

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ print("=" * len(motd))
 
 qty = int(input("quantity of usernames to be generated :"))
 
-def generate_gmail():
+def generate_username():
     first = random.choice(first_name)
     middle = random.choice(middle_name)
     last = random.choice(last_name)
     number = random.randint(10, 99)
-    email = f"{first.lower()}{middle.lower()}{last.lower()}{number}"
-    return email
+    username = f"{first.lower()}{middle.lower()}{last.lower()}{number}"
+    return username
 
 for i in range(qty):
-    print(generate_gmail())
+    print(generate_username())
